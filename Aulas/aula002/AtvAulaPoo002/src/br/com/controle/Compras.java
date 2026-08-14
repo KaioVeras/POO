fd8f38b11cede976b92df2d9a72b73719aec8275
KAIO VÍTOR PINHEIRO VERAS <kaio.veras@a.ucb.br>
2026-08-14 12:51:53 -0300

    POO: Aula 02

---
+++ Aulas/aula002/AtvAulaPoo002/src/br/com/controle/Compras.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/*
+    Compras até 500 - sem desconto
+    Acima de 500 - 10% de desconto
+*/
+
+/**
+ *
+ * @author kaio.veras
+ */
+public class Compras {
+    private double valor;
+    
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    public double getValor() {
+        return valor;
+    }
+    
+//    public double getDesconto() {;
+//        // Calcular desconto de 10%
+//    }
+}
