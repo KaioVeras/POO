f4433da771085056102db00c5b4fc2ba77735b5b
KAIO VÍTOR PINHEIRO VERAS <kaio.veras@a.ucb.br>
2026-08-21 12:21:20 -0300

    POO: Aula 03

--- Aulas/aula003/AulaPoo003/src/br/com/controle/Vendas.java
+++ Aulas/aula003/AulaPoo003/src/br/com/controle/Vendas.java
@@ -1,13 +1,32 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+    Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+    Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.com.controle;
 
 /**
- *
- * @author kaioveras
+    @author kaioveras
  */
 public class Vendas {
+    private double valor;
+            double resposta;
     
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    public double getValor() {
+        return valor;
+    }
+    
+    public double calcularDesconto() {
+        if(this.valor >= 500) {
+            resposta = this.valor - (this.valor * 0.1);
+        } else {
+            resposta = this.valor;
+        }
+        
+        return resposta;
+    }
+
 }
